2923f3759f048ac3cb14b11463e2520a256ec7b0
Ana Maria <naoseidenada0418@gmail.com>
2024-12-11 11:36:26 +0100

    ejercicio modulo  2.3

--- js/main.js
+++ js/main.js
@@ -40,6 +40,12 @@ const buttonAdd = document.querySelector('.js-btn-add');
 const addKitten = document.querySelector('.js-new-form');
 const buttonCancel = document.querySelector('.js-button-cancel');
 const inputReset = document.querySelector('.js-form-input');
+const searchButton = document.querySelector('.js_button-search');
+const inputSearchDesc = document.querySelector('.js_in_search_desc');
+const KittenDesc1 = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente';
+const kittenDesc2 = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraña hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
+const kittenDesc3 = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
+const selectedKitten = document.querySelector('.js-list');
 
 buttonAdd.addEventListener('click', () => {addKitten.classList.toggle('collapsed');
 
@@ -49,5 +55,19 @@ buttonCancel.addEventListener('click', () => {addKitten.classList.add('collapsed
     inputReset.reset();
 });
 
+searchButton.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    const descrSearchText = inputSearchDesc.value;
 
-
+    if (kittenDesc1.includes(descrSearchText)) {
+        console.log(kittenOne);
+      }
+      
+      if (kittenDesc2.includes(descrSearchText)) {
+        //Completa el código
+      }
+      
+      if (kittenDesc3.includes(descrSearchText)) {
+        //Completa el código
+      }
+  });
